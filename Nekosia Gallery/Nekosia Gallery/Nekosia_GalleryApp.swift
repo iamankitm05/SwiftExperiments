a1baa51ad1fbf01bda4e5bd37b4d653fa7bb0612
Ankit kumar <iamankitm05@gmail.com>
2026-04-15 14:05:04 +0530

    Nekosia Gallery Project Setup

---
+++ Nekosia Gallery/Nekosia Gallery/Nekosia_GalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nekosia_GalleryApp.swift
+//  Nekosia Gallery
+//
+//  Created by Ankit Kumar Ojha on 15/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Nekosia_GalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
