027f3bb5bd07c012974368cb7a9b0e41831dec48
Ankit kumar <iamankitm05@gmail.com>
2026-04-21 21:55:12 +0530

    Created services and viewmodels

---
+++ Lazy Loader/Lazy Loader/Views/CharacterCardSkeleton.swift
@@ -0,0 +1,18 @@
+//
+//  CharacterCardSkeleton.swift
+//  Lazy Loader
+//
+//  Created by Ankit Kumar Ojha on 20/04/26.
+//
+
+import SwiftUI
+
+struct CharacterCardSkeleton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CharacterCardSkeleton()
+}
